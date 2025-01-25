1f13f8725423f659934a119983a91ae4f7c6679c
xuguojing <xuguojing77@163.com>
2025-01-25 17:28:31 +0800

    packages shared update

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,9 +1,18 @@
 import { createRequest } from '@vue3-system/axios';
 import type { ResponseData } from '@vue3-system/axios';
 
+// 获取baseURL
+const getBaseURL = () => {
+    try {
+        return typeof import.meta !== 'undefined' ? import.meta.env.VITE_API_BASE_URL : process.env.VITE_API_BASE_URL;
+    } catch {
+        return '';
+    }
+};
+
 // 创建请求实例
 export const request = createRequest({
-    baseURL: import.meta.env.VITE_API_BASE_URL || '',
+    baseURL: getBaseURL() || '',
     timeout: 15000,
 });
 
