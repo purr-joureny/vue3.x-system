f64c68e8b1735ac7236bfa3aff8bdaeda4b8922c
xuguojing <xuguojing77@163.com>
2025-02-26 17:31:01 +0800

    style: update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import * as path from 'path'
-import viteCompression from 'vite-plugin-compression'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import * as path from 'path';
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
